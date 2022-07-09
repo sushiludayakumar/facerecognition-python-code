b5cd97bbf1ba060c9b4d739c20070108d6165cd6
sushiludayakumar <108220128+sushiludayakumar@users.noreply.github.com>
2022-07-09 11:28:04 +0530

    Update Face Recognition.py

--- Face Recognition.py
+++ Face Recognition.py
@@ -2,7 +2,7 @@
 
 import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-img = cv2.imread('sus.jpg')
+img = cv2.imread('face.jpg')
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
